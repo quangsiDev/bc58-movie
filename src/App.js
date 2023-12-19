d31b6f24782d91e51c4fc3fb37805d6400d55f5b
quangsiDev <tranquangsidev@gmail.com>
2023-12-19 18:55:01 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
-
+// git stash
+// git pull
 function App() {
   return (
     <>
