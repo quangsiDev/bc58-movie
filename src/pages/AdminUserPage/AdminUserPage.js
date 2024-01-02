129c3ba763f3d4f022e3b8c8825c1296941f6bf7
quangsiDev <tranquangsidev@gmail.com>
2024-01-02 19:45:35 +0700

    update

--- src/pages/AdminUserPage/AdminUserPage.js
+++ src/pages/AdminUserPage/AdminUserPage.js
@@ -1,52 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { https } from "../../service/api";
-import { Space, Table, Tag } from "antd";
-const columns = [
-  {
-    title: "Account",
-    dataIndex: "taiKhoan",
-    key: "taiKhoan",
-  },
-  {
-    title: "Name",
-    dataIndex: "hoTen",
-    key: "hoTen",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Aciton",
-    key: "action",
-    render: (_) => {
-      return (
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 text-red-500"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
-            />
-          </svg>
-        </button>
-      );
-      // hero icon
-    },
-  },
-];
+import { Space, Table, Tag, message } from "antd";
 
 export default function AdminUserPage() {
   const [listUser, setListUser] = useState([]);
-  useEffect(() => {
+  let fetchUserList = () => {
     https
       .get("/api/QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=GP00")
       .then((res) => {
@@ -56,10 +14,72 @@ export default function AdminUserPage() {
       .catch((err) => {
         console.log(err);
       });
+  };
+  useEffect(() => {
+    fetchUserList();
   }, []);
+  const columns = [
+    {
+      title: "Account",
+      dataIndex: "taiKhoan",
+      key: "taiKhoan",
+    },
+    {
+      title: "Name",
+      dataIndex: "hoTen",
+      key: "hoTen",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Aciton",
+      key: "action",
+      render: (_, record) => {
+        return (
+          <button
+            onClick={() => {
+              handleDelete(record.taiKhoan);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-red-500"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+              />
+            </svg>
+          </button>
+        );
+        // hero icon
+      },
+    },
+  ];
+  let handleDelete = (id) => {
+    https
+      .delete(`/api/QuanLyNguoiDung/XoaNguoiDung?TaiKhoan=${id}`)
+      .then((res) => {
+        console.log(res);
+        fetchUserList();
+        message.success("success");
+      })
+      .catch((err) => {
+        message.error(err.response.data.content);
+      });
+  };
   return (
     <div>
       <Table columns={columns} dataSource={listUser} />
     </div>
   );
 }
+// /
--- src/service/api.js
+++ src/service/api.js
@@ -10,6 +10,7 @@ export let https = axios.create({
   headers: {
     TokenCybersoft:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NCIsIkhldEhhblN0cmluZyI6IjE0LzA1LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNTY0NDgwMDAwMCIsIm5iZiI6MTY4NzcxMjQwMCwiZXhwIjoxNzE1NzkyNDAwfQ.cy8EAM6hrKh2o6c9THZW5lrKeOEmQXIDgFVyIf7K_rU",
+    Authorization: "bearer " + JSON.parse(localStorage.getItem("USER_INFOR"))?.accessToken,
   },
 });
 // useDispatch();
