80b77da3431ad3aa36e0424eebd4c3267bfaabc6
quangsiDev <tranquangsidev@gmail.com>
2024-01-16 20:25:53 +0700

    update

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import FormLogin from "./FormLogin";
-
+import Lottie from "lottie-react";
+import animateSrc from "./bgAnimate.json";
 export default function LoginPage() {
   return (
-    <div className="container">
+    <div className="container flex">
       <FormLogin />
+      <div className="w-1/2">
+        <Lottie animationData={animateSrc} loop={true} />
+      </div>
     </div>
   );
 }
